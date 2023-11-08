229acf965d63d32cfeff0e8f97e186836b0156c4
root <you@example.com>
2023-11-08 05:38:29 -0800

    nf

--- 0-print_name.c/0-print_name.c
+++ 0-print_name.c/0-print_name.c
@@ -6,11 +6,8 @@
 * @f: pointer to function
 * Return: nothing
 **/
-	void print_name(char *name, void (*f)(char *))
-	{
-		if (name == NULL || f == NULL)
-			return;
-
-
+void print_name(char *name, void (*f)(char *))
+{
+		if (name && f)
 		f(name);
-	}
+}	}
